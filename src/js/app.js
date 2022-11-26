38fe36e749f6279a456070af98a1f8dfb6f41a35
NikolovToni <nikolov.s.toni@gmail.com>
2022-11-26 14:12:21 +0200

    moving the js code to app.js

--- src/js/app.js
+++ src/js/app.js
@@ -2,4 +2,8 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
+  const hotClasses = document.querySelectorAll(".hot");
+  hotClasses.forEach((emoji) => {
+    emoji.innerHTML += String.fromCodePoint(0x1f525);
+  });
 });
